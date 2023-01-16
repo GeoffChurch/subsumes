45d53a1dcc7b59d9bee0fda2b7f6ced57898ce86
Geoffrey Churchill <geoffrey.a.churchill@gmail.com>
2023-01-15 23:43:34 -0500

    Add requires(clpfd).

--- pack.pl
+++ pack.pl
@@ -3,3 +3,4 @@ version('0.1').
 title('Relational term subsumption. subsumes/2 is intended as a relational drop-in replacement for subsumes_term/2. Note that this conflicts with the deprecated predicate terms:subsumes/2.').
 author('Geoffrey Churchill', 'geoffrey.a.churchill@gmail.com').
 home('https://github.com/GeoffChurch/subsumes').
+requires(clpfd).
