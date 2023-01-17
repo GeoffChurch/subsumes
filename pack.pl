5e1bc6d743319623c980aeba69cd47c395f8c65c
Geoffrey Churchill <geoffrey.a.churchill@gmail.com>
2023-01-16 23:16:37 -0500

    Bump version 0.1 => 0.2

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(subsumes).
-version('0.1').
+version('0.2').
 title('Relational term subsumption. subsumes/2 is intended as a relational drop-in replacement for subsumes_term/2. Note that this conflicts with the deprecated predicate terms:subsumes/2.').
 author('Geoffrey Churchill', 'geoffrey.a.churchill@gmail.com').
 home('https://github.com/GeoffChurch/subsumes').
